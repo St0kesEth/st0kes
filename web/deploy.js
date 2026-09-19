f21e6b60ce7c30f9deef95f4b6623b60fd8e2e9c
St0kesEth <283016779+St0kesEth@users.noreply.github.com>
2026-09-19 22:54:06 +0000

    web/deploy.js: deployed to Robinhood Chain 4663; Engine 0x2938..., Live 0xEC63...

--- web/deploy.js
+++ web/deploy.js
@@ -1,2 +1,21 @@
-// Written by the deploy step; null while the contracts are not on the chain.
-export default null;
+// Written by the deploy step, holds the addresses of the two contracts.
+export default {
+  "engine": "0x29381810B485566206e216a8aC4De8F963C0cEAf",
+  "live": "0xEC637733bE276dB2ab3D82Fc1830b2128B430e13",
+  "chainId": 4663,
+  "explorer": "https://explorer.mainnet.chain.robinhood.com",
+  "block": "0x4054438",
+  "deployedAt": "2026-09-19T22:52:09Z",
+  "txs": [
+    {
+      "name": "Engine",
+      "hash": "0x7f7443661dcafd360b52439121838f244d45facfde50975307b1fe2eff76d9db",
+      "gas": "0x7d348"
+    },
+    {
+      "name": "Live",
+      "hash": "0x8d58048396c54b710970e8b9fe37909efe2c56598f7b5082a8651b3f70328df5",
+      "gas": "0x1c9be8"
+    }
+  ]
+};
